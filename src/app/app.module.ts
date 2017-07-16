b9c0c0f6269c6c2f7cb6834cc2f83c995375d92f
MeesterMarcus <lorenzanamarcus@gmail.com>
2017-07-15 23:35:43 -0500

    styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdButtonModule, MdCheckboxModule} from '@angular/material';
+import 'hammerjs';
+
 import { AppComponent } from './app.component';
 import { NewsfeedComponent } from './newsfeed/newsfeed.component';
 
@@ -10,7 +14,10 @@ import { NewsfeedComponent } from './newsfeed/newsfeed.component';
     NewsfeedComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
